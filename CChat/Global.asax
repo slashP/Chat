700bf5554bc438d97b792e2c97e53d7d725f1d9b
Per-Kristian Helland <perkristianhelland@gmail.com>
2012-07-04 00:55:05 +0200

    retry

--- CChat/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CChat.MvcApplication" Language="C#" %>
--- ChatApp/Properties/AssemblyInfo.cs
+++ ChatApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CChat")]
+[assembly: AssemblyTitle("ChatApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("CChat")]
+[assembly: AssemblyProduct("ChatApp")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7a75ab73-96f5-47c9-92f5-48202327dda6")]
+[assembly: Guid("11238ede-9fb2-4e02-bc2e-abc8f9d9bf83")]
 
 // Version information for an assembly consists of the following four values:
 //
