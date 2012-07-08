76061d8d165244d75a2ced4aac1f4069093f5922
Per-Kristian Helland <perkristianhelland@gmail.com>
2012-07-08 17:34:05 +0200

    Mobile views

--- ChatApp/Global.asax.cs
+++ ChatApp/Global.asax.cs
@@ -1,20 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.WebPages;
+using ChatApp.Util;
 
 namespace ChatApp
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
@@ -24,6 +15,13 @@ namespace ChatApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DisplayModeProvider.Instance.Modes.Insert(0, new MobileDisplayMode());
+            DisplayModeProvider.Instance.Modes.Insert(1, new DefaultDisplayMode("Mobile")
+            {
+                ContextCondition = context =>
+                    context.GetOverriddenBrowser().IsMobileDevice
+            }); 
+
         }
     }
 }
---
+++ ChatApp/Util/MobileDisplayMode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web.WebPages;
+
+namespace ChatApp.Util
+{
+    public class MobileDisplayMode : DefaultDisplayMode
+    {
+        private readonly StringCollection _useragenStringPartialIdentifiers = new StringCollection
+    {
+        "Android",
+        "Mobile",
+        "Opera Mobi",
+        "Samsung",
+        "HTC",
+        "Nokia",
+        "Ericsson",
+        "SonyEricsson",
+        "iPhone",
+    };
+
+        public MobileDisplayMode()
+            : base("Mobile")
+        {
+            ContextCondition = (context => IsMobile(context.GetOverriddenUserAgent()));
+        }
+
+        private bool IsMobile(string useragentString)
+        {
+            return _useragenStringPartialIdentifiers.Cast<string>()
+                        .Any(val => useragentString.IndexOf(val, StringComparison.InvariantCultureIgnoreCase) >= 0);
+        }
+    }
+}
