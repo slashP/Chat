259cc1b9a7f01c8e5a50a910a815d09973253e36
Per-Kristian Helland <perkristianhelland@gmail.com>
2012-07-08 20:26:00 +0200

    Online user liste

--- ChatApp/Controllers/Chat.cs
+++ ChatApp/Controllers/Chat.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Web;
+using ChatApp.Util;
 using CiberChat.Models;
 using SignalR.Hubs;
 
 namespace CiberChat.Controllers
 {
-    public class Chat : Hub
+    public class Chat : Hub, IConnected, IDisconnect
     {
         private readonly DataContext _db = new DataContext();
+        private const string DateTimeFormat = "d.MMMM HH:mm:ss";
+        private static readonly Dictionary<string, string> _clients = new Dictionary<string, string>();
 
         public void GetAllMessages()
         {
@@ -20,7 +24,7 @@ namespace CiberChat.Controllers
 
             var jsonMessages = (from chatMessage in messages
                      let time = chatMessage.Time.HasValue
-                                    ? ((DateTime) chatMessage.Time).ToString("d.MMMM HH:mm:ss", new CultureInfo("nb-NO"))
+                                    ? ((DateTime) chatMessage.Time).ToString(DateTimeFormat, new CultureInfo("nb-NO"))
                                     : string.Empty
                      select new
                          {
@@ -41,7 +45,84 @@ namespace CiberChat.Controllers
             chatMessage.Time = DateTime.UtcNow.AddHours(2);
             _db.ChatMessages.Add(chatMessage);
             _db.SaveChanges();
-            Clients.addMessage(new{Time = DateTime.UtcNow.AddHours(2).ToString("d.MMMM HH:mm:ss"), chatMessage.User, chatMessage.Message});
+            Clients.addMessage(new{Time = DateTime.UtcNow.AddHours(2).ToString(DateTimeFormat, new CultureInfo("nb-NO")), chatMessage.User, chatMessage.Message});
+        }
+
+        public Task Connect()
+        {
+            var user = GetCurrentUser();
+            if (user != null)
+            {
+                string newUser;
+
+                if (!_clients.TryGetValue(Context.ConnectionId, out newUser))
+                {
+                    _clients.Add(Context.ConnectionId, user);
+                }
+            }
+            return Clients.updateOnlineUsers(GetConnectedUsersList());
+        }
+
+        private string GetCurrentUser()
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(Context.User.Identity.Name))
+                {
+                    return Context.User.Identity.Name;
+                }
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("User not logged in.");
+            }
+            return null;
+        }
+
+        public Task Reconnect(IEnumerable<string> groups)
+        {
+            var user = GetCurrentUser();
+            if (user != null)
+            {
+                lock (_clients)
+                {
+                    string newUser;
+                    if (!_clients.TryGetValue(Context.ConnectionId, out newUser))
+                    {
+                        _clients.Add(Context.ConnectionId, user);
+                        return Clients.updateOnlineUsers(GetConnectedUsersList());
+                    } 
+                }
+            }
+            return new Task(() => {});
+        }
+
+        public Task Disconnect()
+        {
+            var user = GetCurrentUser();
+            if(user != null)
+            {
+                lock (_clients)
+                {
+                    string newUser;
+                    if (!_clients.TryGetValue(Context.ConnectionId, out newUser))
+                    {
+                        _clients.Remove(Context.ConnectionId);
+                        return Clients.updateOnlineUsers(GetConnectedUsersList());
+                    }
+                }
+            }
+            return new Task(() => {});
+        }
+
+        private static IEnumerable<OnlineUser> GetConnectedUsersList()
+        {
+            return _clients.Values.Select(x => new OnlineUser {username = x}).ToList().DistinctBy(x => x.username);
+        }
+
+        public class OnlineUser
+        {
+            public string username { get; set; }
         }
     }
 }
